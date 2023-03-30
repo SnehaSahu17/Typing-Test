c787fc047c54ec322a6371e0d8b975d38070e176
Sneha Sahu <snehasahu@Snehas-MacBook-Air.local>
2023-03-30 17:25:38 +0530

    ft:error removed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Random Quotes Api URL
 const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
 const quoteSection = document.getElementById("quote");
 const userInput = document.getElementById("quote-input");
@@ -6,29 +7,39 @@ let time = 60;
 let timer = "";
 let mistakes = 0;
 
+//Display random quotes
 const renderNewQuote = async () => {
-  const response = await fetch(quoteApiUrl); //fetch content from API
-  let data = await response.json(); //store content
-  quote = data.content;
+  const response = await fetch(quoteApiUrl); //Fetch contents from url
+
+  let data = await response.json(); //Store response
+
+  quote = data.content; //Access quote
+
+  //Array of characters in the quote
   let arr = quote.split("").map((value) => {
     return "<span class='quote-chars'>" + value + "</span>";
   });
-  quoteSection.innerHTML += arr.join(""); //display quote
+
+  quoteSection.innerHTML += arr.join("");
 };
 
 //Logic for comparing input words with quote
 userInput.addEventListener("input", () => {
-  //array of quote
   let quoteChars = document.querySelectorAll(".quote-chars");
+  //Create an array from quoted text
   quoteChars = Array.from(quoteChars);
-  //array of user-input
+
+  //array of user input characters
   let userInputChars = userInput.value.split("");
+
+  //loop through each character in quote
   quoteChars.forEach((char, index) => {
     if (char.innerText == userInputChars[index]) {
       char.classList.add("success");
     }
     //If user hasn't entered anything or backspaced
     else if (userInputChars[index] == null) {
+      //Remove class if any
       if (char.classList.contains("success")) {
         char.classList.remove("success");
       } else {
@@ -37,162 +48,43 @@ userInput.addEventListener("input", () => {
     }
     //If user enter wrong character
     else {
-      //Checks if we alreasy have added fail class
       if (!char.classList.contains("fail")) {
         //increment and display mistakes
-        mistakes += 1;//Random Quotes Api URL
-        const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
-        const quoteSection = document.getElementById("quote");
-        const userInput = document.getElementById("quote-input");
-        let quote = "";
-        let time = 60;
-        let timer = "";
-        let mistakes = 0;
-        
-        //Display random quotes
-        const renderNewQuote = async () => {
-          //Fetch contents from url
-          const response = await fetch(quoteApiUrl);
-        
-          //Store response
-          let data = await response.json();
-        
-          //Access quote
-          quote = data.content;
-        
-          //Array of characters in the quote
-          let arr = quote.split("").map((value) => {
-            //wrap the characters in a span tag
-            return "<span class='quote-chars'>" + value + "</span>";
-          });
-          //join array for displaying
-          quoteSection.innerHTML += arr.join("");
-        };
-        
-        //Logic for comparing input words with quote
-        userInput.addEventListener("input", () => {
-          let quoteChars = document.querySelectorAll(".quote-chars");
-          //Create an arrat from received span tags
-          quoteChars = Array.from(quoteChars);
-        
-          //array of user input characters
-          let userInputChars = userInput.value.split("");
-        
-          //loop through each character in quote
-          quoteChars.forEach((char, index) => {
-            //Check if char(quote character) = userInputChars[index](input character)
-            if (char.innerText == userInputChars[index]) {
-              char.classList.add("success");
-            }
-            //If user hasn't entered anything or backspaced
-            else if (userInputChars[index] == null) {
-              //Remove class if any
-              if (char.classList.contains("success")) {
-                char.classList.remove("success");
-              } else {
-                char.classList.remove("fail");
-              }
-            }
-            //If user enter wrong character
-            else {
-              //Checks if we alreasy have added fail class
-              if (!char.classList.contains("fail")) {
-                //increment and display mistakes
-                mistakes += 1;
-                char.classList.add("fail");
-              }
-              document.getElementById("mistakes").innerText = mistakes;
-            }
-            //Returns true if all the characters are entered correctly
-            let check = quoteChars.every((element) => {
-              return element.classList.contains("success");
-            });
-            //End test if all characters are correct
-            if (check) {
-              displayResult();
-            }
-          });
-        });
-        
-        //Update Timer on screen
-        function updateTimer() {
-          if (time == 0) {
-            //End test if timer reaches 0
-            displayResult();
-          } else {
-            document.getElementById("timer").innerText = --time + "s";
-          }
-        }
-        
-        //Sets timer
-        const timeReduce = () => {
-          time = 60;
-          timer = setInterval(updateTimer, 1000);
-        };
-        
-        //End Test
-        const displayResult = () => {
-          //display result div
-          document.querySelector(".result").style.display = "block";
-          clearInterval(timer);
-          document.getElementById("stop-test").style.display = "none";
-          userInput.disabled = true;
-          let timeTaken = 1;
-          if (time != 0) {
-            timeTaken = (60 - time) / 100;
-          }
-          document.getElementById("wpm").innerText =
-            (userInput.value.length / 5 / timeTaken).toFixed(2) + " wpm";
-          document.getElementById("accuracy").innerText =
-            Math.round(
-              ((userInput.value.length - mistakes) / userInput.value.length) * 100
-            ) + " %";
-        };
-        
-        //Start Test
-        const startTest = () => {
-          mistakes = 0;
-          timer = "";
-          userInput.disabled = false;
-          timeReduce();
-          document.getElementById("start-test").style.display = "none";
-          document.getElementById("stop-test").style.display = "block";
-        };
-        
-        window.onload = () => {
-          userInput.value = "";
-          document.getElementById("start-test").style.display = "block";
-          document.getElementById("stop-test").style.display = "none";
-          userInput.disabled = true;
-          renderNewQuote();
-        };
+        mistakes += 1;
         char.classList.add("fail");
       }
       document.getElementById("mistakes").innerText = mistakes;
     }
+    //Returns true if all the characters are entered correctly
     let check = quoteChars.every((element) => {
       return element.classList.contains("success");
     });
-    //End test if all characters are correct
+
     if (check) {
       displayResult();
     }
   });
 });
-//update timer on screen
+
+//Update Timer on screen
 function updateTimer() {
-  if (time == 0) displayResult(); //end test
-  else document.getElementById("timer").innerText = --time + "s";
+  if (time == 0) {
+    //End test if timer reaches 0
+    displayResult();
+  } else {
+    document.getElementById("timer").innerText = --time + "s";
+  }
 }
-//sets time
+
+//Sets timer
 const timeReduce = () => {
   time = 60;
   timer = setInterval(updateTimer, 1000);
 };
 
-//end test
-
+//End Test
 const displayResult = () => {
+  //display result
   document.querySelector(".result").style.display = "block";
   clearInterval(timer);
   document.getElementById("stop-test").style.display = "none";
@@ -204,14 +96,14 @@ const displayResult = () => {
   //words per minute
   document.getElementById("wpm").innerText =
     (userInput.value.length / 5 / timeTaken).toFixed(2) + " wpm";
-  //accuracy
+  //accuracy calculated
   document.getElementById("accuracy").innerText =
     Math.round(
       ((userInput.value.length - mistakes) / userInput.value.length) * 100
     ) + " %";
 };
 
-//start test
+//Start Test
 const startTest = () => {
   mistakes = 0;
   timer = "";
